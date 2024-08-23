b631c4f0a44fd364cc018a2b57a448cb4c0d757a
Chechobuitrago <buitrago12.09@gmail.com>
2024-08-23 11:25:25 -0500

    Initial Commit

---
+++ SwiftUICrypto/SwiftUICryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICryptoApp.swift
+//  SwiftUICrypto
+//
+//  Created by Sergio Buitrago on 23/08/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
