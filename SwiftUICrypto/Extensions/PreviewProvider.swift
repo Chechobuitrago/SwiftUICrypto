d4014d153ee34a2b383dc1b4049b6369f7a56349
Chechobuitrago <buitrago12.09@gmail.com>
2024-08-24 18:39:45 -0500

    CoinRowView

--- SwiftUICrypto/Extensions/PreviewProvider.swift
+++ SwiftUICrypto/Extensions/PreviewProvider.swift
@@ -7,26 +7,27 @@
 
 import Foundation
 import SwiftUI
-/*
-extension PreviewProvider {
-    
-    static var dev: DeveloperPreview {
-        return DeveloperPreview.instance
-    }
-    
-}
+
+//extension PreviewProvider {
+//    
+//    static var dev: DeveloperPreview {
+//        return DeveloperPreview.instance
+//    }
+//    
+//}
 
 class DeveloperPreview {
     
     static let instance = DeveloperPreview()
     private init() { }
     
+    /*
     let homeVM = HomeViewModel()
     
     let stat1 = StatisticModel(title: "Market Cap", value: "$12.5Bn", percentageChange: 25.34)
     let stat2 = StatisticModel(title: "Total Volume", value: "$1.23Tr")
     let stat3 = StatisticModel(title: "Portfolio Value", value: "$50.4k", percentageChange: -12.34)
-    
+    */
     let coin = CoinModel(
        id: "bitcoin",
        symbol: "btc",
@@ -228,4 +229,4 @@ class DeveloperPreview {
        currentHoldings: 1.5)
     
 }
-*/
+
