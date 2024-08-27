5e3c8ef221c4e38d8368406f2dd03988ed2cf61b
Chechobuitrago <buitrago12.09@gmail.com>
2024-08-27 10:12:45 -0500

    CoinImage LOGIC

---
+++ SwiftUICrypto/Core/Components/CoinImage/CoinImageView.swift
@@ -0,0 +1,40 @@
+//
+//  CoinImageView.swift
+//  SwiftUICrypto
+//
+//  Created by Sergio Buitrago on 27/08/24.
+//
+
+import SwiftUI
+
+
+
+struct CoinImageView: View {
+    
+    @StateObject var vm: CoinImageViewModel
+    
+    init(coin: CoinModel) {
+        _vm = StateObject(wrappedValue: CoinImageViewModel(coin: coin))
+    }
+    
+    var body: some View {
+        ZStack {
+            if let image = vm.image {
+                Image(uiImage: image)
+                    .resizable()
+                    .scaledToFit()
+            } else if vm.isLoading {
+                ProgressView()
+            } else {
+                Image(systemName: "questionmark")
+                    .foregroundStyle(Color.theme.secondaryText)
+            }
+        }
+    }
+}
+
+#Preview {
+    CoinImageView(coin: DeveloperPreview.instance.coin)
+        .padding()
+        .previewLayout(.sizeThatFits)
+}
